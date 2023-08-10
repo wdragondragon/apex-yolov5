ece187b98cfd9e5e639217879fa98d1cd9be3bf4
zhengjinlong <jdragon9527@gmail.com>
2023-08-10 14:57:35 +0800

    feat: 多线程server client

--- apex_yolov5/LogUtil.py
+++ apex_yolov5/LogUtil.py
@@ -1,13 +1,28 @@
 from apex_yolov5.LogWindow import LogWindow
 
+
+class LogUtil:
+
+    def __init__(self):
+        self.use_time_dict = dict()
+
+    def set_time(self, use_time_type, use_time):
+        self.use_time_dict[use_time_type] = self.use_time_dict.get(use_time_type, 0) + use_time
+
+    def print_time(self, print_count):
+        for k, v in self.use_time_dict.items():
+            LogWindow().print_log("步骤[{}]使用平均时间:{}ms".format(k, v * 1000 / print_count))
+        self.use_time_dict.clear()
+
+
 use_time_dict = dict()
 
 
-def set_time(use_time_type, use_time):
-    use_time_dict[use_time_type] = use_time_dict.get(use_time_type, 0) + use_time
+def set_time(self, use_time_type, use_time):
+    self.use_time_dict[use_time_type] = self.use_time_dict.get(use_time_type, 0) + use_time
 
 
-def print_time(print_count):
-    for k, v in use_time_dict.items():
+def print_time(self, print_count):
+    for k, v in self.use_time_dict.items():
         LogWindow().print_log("步骤[{}]使用平均时间:{}ms".format(k, v * 1000 / print_count))
-    use_time_dict.clear()
+    self.use_time_dict.clear()
