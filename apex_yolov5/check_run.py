63e99f83c3410b89e7e75ef8f49a32f316672d96
zhengjinlong <jdragon9527@gmail.com>
2023-09-01 14:48:34 +0800

    feat: 获取其他电脑硬件信息

--- apex_yolov5/check_run.py
+++ apex_yolov5/check_run.py
@@ -1,11 +1,73 @@
-import uuid
-import os
-import requests
 import sys
+
+import requests
 import uuid
 
+import requests
+import wmi
 
-# 获取机器码（这里以MAC地址作为机器码）
+s = wmi.WMI()
+
+
+def get_device_id():
+    for item in s.Win32_ComputerSystemProduct():
+        return item.UUID
+
+
+# cpu 序列号
+def get_CPU_info():
+    cpu = []
+    cp = s.Win32_Processor()
+    for u in cp:
+        cpu.append(
+            {
+                "Name": u.Name,
+                "Serial Number": u.ProcessorId,
+                "CoreNum": u.NumberOfCores
+            }
+        )
+    return cpu
+
+
+# 硬盘序列号
+def get_disk_info():
+    disk = []
+    for pd in s.Win32_DiskDrive():
+        disk.append(
+            {
+                "Serial": s.Win32_PhysicalMedia()[0].SerialNumber.lstrip().rstrip(),  # 获取硬盘序列号，调用另外一个win32 API
+                "ID": pd.deviceid,
+                "Caption": pd.Caption,
+                "size": str(int(float(pd.Size) / 1024 / 1024 / 1024)) + "G"
+            }
+        )
+    return disk
+
+
+# mac 地址（包括虚拟机的）
+def get_network_info():
+    network = []
+    for nw in s.Win32_NetworkAdapterConfiguration():  # IPEnabled=0
+        if nw.MACAddress != None:
+            network.append(
+                {
+                    "MAC": nw.MACAddress,  # 无线局域网适配器 WLAN 物理地址
+                    "ip": nw.IPAddress
+                }
+            )
+    return network
+
+
+# 主板序列号
+def get_mainboard_info():
+    mainboard = []
+    for board_id in s.Win32_BaseBoard():
+        mainboard.append(board_id.SerialNumber.strip().strip('.'))
+    return mainboard
+
+    # 获取机器码（这里以MAC地址作为机器码）
+
+
 def get_machine_code():
     mac = uuid.getnode()
     return ':'.join(('%012X' % mac)[i:i + 2] for i in range(0, 12, 2))
