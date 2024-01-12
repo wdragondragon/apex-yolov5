7aa869acb118591c06fe57914bc2cb55ca5dd150
zhengjinlong <1061917196@qq.com>
2024-01-12 17:23:13 +0800

    feat: 优化鼠标模块代码

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
     log_window = ConfigWindow(global_config)
     dis = DisclaimerWindow(log_window)
-    # check_run.check(log_window)
+    check_run.check(log_window)
 
     MoverFactory.init_mover(
         mouse_model=global_config.mouse_model,
